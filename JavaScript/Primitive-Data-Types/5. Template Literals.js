3e591afa64bdf9032f626d0f01a33135be95933a
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2024-04-14 14:33:28 +0300

    5. Template Literals.js

---
+++ JavaScript/Primitive-Data-Types/5. Template Literals.js
@@ -0,0 +1,31 @@
+//
+const name = "Amin";
+const age = 50;
+console.log(`My name is ${name} and I am ${age} years old.\n`);
+
+//
+const length = 5;
+const width = 3;
+const area = length * width;
+console.log(
+  `The area of the rectangle with length of ${length} and width of ${width} is ${area}.\n`
+);
+
+//
+const today = "Sunday";
+const task1 = "finish the udemy videos";
+const task2 = "finish the homeworks";
+const rask = "add some extensions to vscode";
+console.log(`On day ${today}, I will ${task1}, ${task2}, and ${rask}.\n`);
+
+//
+numberOfPetsIHave = 1;
+console.log(`I have ${numberOfPetsIHave} pets.\n`);
+
+//
+const base = 6;
+const height = 8;
+const areaOfTriangle = (base * height) / 2;
+console.log(
+  `The area of the triangle with base of ${base} and height of ${height} is ${areaOfTriangle}.`
+);
