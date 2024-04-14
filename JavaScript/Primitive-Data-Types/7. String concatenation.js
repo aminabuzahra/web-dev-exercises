3c0672b09997fe02a8f9b3ffebf33d123952facd
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2024-04-14 23:23:02 +0300

    7. String concatenation.js

---
+++ JavaScript/Primitive-Data-Types/7. String concatenation.js
@@ -0,0 +1,38 @@
+// 1. Introduce Yourself
+const name = "Amin";
+const age = 50;
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+// 2. Favorite Things
+const favoriteColor = "Dark Grape";
+const favoriteMovie = "Avatar I";
+const favoriteFood = "Stuffed Turnip with Tamarind Sauce";
+console.log(
+  "My favorite color is " +
+    favoriteColor +
+    ", I love the movie " +
+    favoriteMovie +
+    ", and I enjoy eating " +
+    favoriteFood +
+    "."
+);
+
+// 3. Simple Math
+const a = 5;
+const b = 3;
+const result = a + b;
+console.log(
+  "The sum of " +
+    a +
+    " and " +
+    b +
+    " is " +
+    result +
+    " where " +
+    result +
+    " is the result of sum a and b."
+);
+
+// 4. Day of the Week
+const currentDay = "Sunday";
+console.log("Today is " + currentDay + ".");
