67262288525e432ba15b88527ee69b2e99d855de
Amin Abu-Zahra <amin.abuzahra@gmail.com>
2024-05-01 14:33:07 +0300

    17. Arrow Function.js

--- JavaScript/RPG Game Project/17. Arrow Functions.js
+++ JavaScript/RPG Game Project/17. Arrow Functions.js
@@ -19,19 +19,24 @@ const player = {
 
 // 1. Simple Arrow Function Conversion
 console.log("1. Simple Arrow Function Conversion");
+console.log("==================================");
+console.log();
+
 function getPlayerName(player) {
   return player.name;
 }
 
-const getPlayerNameArrow = (player) => player.name;
+const getPlayerNameArrow = (player) => {
+  return player.name;
+};
+
 console.log(getPlayerNameArrow(player));
 
-
-
-
-
-
 // 2. Control Flow in Arrow Functions
+console.log("2. Control Flow in Arrow Functions");
+console.log("==================================");
+console.log();
+
 function getPlayerHealth(health) {
   if (health > 70) {
     return "High";
@@ -42,7 +47,22 @@ function getPlayerHealth(health) {
   }
 }
 
+const getPlayerHealthArrow = (health) => {
+  if (health > 70) {
+    return "High";
+  } else if (health > 40) {
+    return "Medium";
+  } else {
+    return "Low";
+  }
+};
+
+console.log(getPlayerHealthArrow(player.health));
+
 // 3. Arrow Functions with Multiple Operations
+console.log("3. Arrow Functions with Multiple Operations");
+console.log("===========================================");
+console.log();
 function getTotalInventoryValue(items) {
   let totalValue = 0;
   for (let i = 0; i < items.length; i++) {
@@ -51,14 +71,37 @@ function getTotalInventoryValue(items) {
   return "Total inventory value is " + totalValue;
 }
 
+const getTotalInventoryValueArrow = (items) => {
+  let totalValue = 0;
+  for (let i = 0; i < items.length; i++) {
+    totalValue += items[i].value;
+  }
+  return "Total inventory value is " + totalValue;
+};
+
+console.log(getTotalInventoryValueArrow(player.items));
+
 // 4. Arrow Functions as Callbacks
-const sortedItems = items.sort(function (a, b) {
+console.log("4. Arrow Functions as Callbacks");
+console.log("===============================");
+console.log();
+
+const sortedItems = player.items.sort(function (a, b) {
   return b.value - a.value;
 });
 
+const sortedItemsArrow = player.items.sort((a, b) => a.value - b.value);
+console.log(sortedItemsArrow);
+
 // 5. Implicit Return with Arrow Functions
+console.log("5. Implicit Return with Arrow Functions");
+console.log("=======================================");
+console.log();
 function getItemNames(items) {
   return items.map(function (item) {
     return item.name;
   });
 }
+
+const getItemNamesArrow = (items) => items.map((item) => item.name);
+console.log(getItemNamesArrow(player.items));
